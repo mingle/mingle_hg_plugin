b1ed038b4005178569921527339387fb244e4742
smarker <smarker@thoughtworks.com>
2012-05-11 15:00:06 -0700

    #13478 - Tfs project repository connection setting values are lost on error - Moved the show, update and create actions out of the plugins to the repository_controller in the mingle code base

--- rails_plugin/app/controllers/hg_configurations_controller.rb
+++ rails_plugin/app/controllers/hg_configurations_controller.rb
@@ -1,14 +1,10 @@
 #Copyright 2010 ThoughtWorks, Inc. Licensed under the Apache License, Version 2.0.
 
 class HgConfigurationsController < ProjectApplicationController
-  
-  #<snippet name="method_verification">
+
   verify :method => :post, :only => [ :save, :create ]
   verify :method => :put, :only => [ :update ]
-  #</snippet>
-  #<snippet name="privileges">
   privileges UserAccess::PrivilegeLevel::PROJECT_ADMIN => ['index', 'save', 'update', 'show', 'create']
-  #</snippet>
 
   def current_tab
     Project::ADMIN_TAB_INFO
@@ -17,55 +13,5 @@ class HgConfigurationsController < ProjectApplicationController
   def admin_action_name
     super.merge(:controller => 'repository')
   end
-  #</snippet>
 
-  def show
-    @hg_configuration = HgConfiguration.find(:first, :conditions => ["marked_for_deletion = ? AND project_id = ?", false, @project.id])
-    #<snippet name="handling_xml_call">
-    respond_to do |format|
-      format.xml do
-        if @hg_configuration 
-          render :xml => @hg_configuration.to_xml
-        else
-          render :xml => "No Mercurial configuration found in project #{@project.identifier}.", :status => 404
-        end
-      end
-    end
-    #</snippet>
-  end
-
-  def update
-    @hg_configuration = HgConfiguration.find_by_id(params[:id])
-    create_or_update if @hg_configuration
-    
-    respond_to do |format|
-      format.xml do
-        if @hg_configuration.nil?
-          render :xml => "No Mercurial configuration found in project #{@project.identifier} with id #{params[:id]}.", :status => 404
-        elsif @hg_configuration.errors.empty?
-          render :xml => @hg_configuration.to_xml
-        else
-          render :xml => @hg_configuration.errors.to_xml, :status => 422
-        end
-      end
-    end
-  end
-
-  def create
-    create_or_update
-    respond_to do |format|
-      format.xml do
-        if @hg_configuration.errors.empty?
-          head :created, :location => url_for(:action => @hg_configuration.id, :format => :xml)
-        else
-          render :xml => @hg_configuration.errors.to_xml, :status => 422
-        end
-      end
-    end
-  end
-
-  def create_or_update
-    @hg_configuration = HgConfiguration.create_or_update(@project.id, params[:id], params[:hg_configuration])
-  end
-    
 end
